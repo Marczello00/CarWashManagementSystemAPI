02d94608247b2cd5e39aee14a46687cd04804c01
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2024-07-16 00:48:29 +0200

    Added AutoMapper and made webserver available in LAN for testing purposes

---
+++ Helpers/MappingProfiles.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CarWashManagementSystem.Dtos;
+using CarWashManagementSystem.Models;
+
+namespace CarWashManagementSystem.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Transaction, TransactionDto>()
+                .ForMember(dest => dest.Station, opt => opt.MapFrom(src => src.Station))
+                .ForMember(dest => dest.TransactionSource, opt => opt.MapFrom(src => src.TransactionSource));
+
+            CreateMap<Station, StationDto>()
+                .ForMember(dest => dest.StationType, opt => opt.MapFrom(src => src.StationType));
+
+            CreateMap<TransactionSource, TransactionSourceDto>();
+
+            CreateMap<StationType, StationTypeDto>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Automapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,7 +22,11 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 }
 );
-
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5154); // HTTP
+    options.ListenAnyIP(7115, listenOptions => listenOptions.UseHttps()); // HTTPS
+});
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -37,6 +45,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.UseAuthorization();
 
