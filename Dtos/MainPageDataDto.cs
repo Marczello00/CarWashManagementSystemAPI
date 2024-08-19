a1662ddca4c33a6d75570023dc2a352d8ef9c3ed
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2024-08-19 22:22:39 +0200

    Created Controller for main page of client app

---
+++ Dtos/MainPageDataDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CarWashManagementSystem.Dtos
+{
+    public class MainPageDataDto
+    {
+        public int Id { get; set; }
+        public string StationName { get; set; }
+        public int TodaysRevenue { get; set; }
+        public bool IsStationWorking { get; set; }
+        public bool IsTaxingEnabled { get; set; }
+        public string IpAddress { get; set; }
+        public short StationNumber { get; set; }
+        public string StationTypeName { get; set; }
+
+    }
+}
